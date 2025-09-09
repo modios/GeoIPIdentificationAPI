023034fa5bf3fc149adb88672f62d71138fe6dbd
Anastasios Karangelis <akarangelis@arrowresources.com>
2025-09-09 11:07:40 +0300

    GEOAPI-2 Add Serilog and Swagger

--- GeoIPIdentification/GeoIPIdentification.API/Program.cs
+++ GeoIPIdentification/GeoIPIdentification.API/Program.cs
@@ -1,11 +1,28 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .Enrich.FromLogContext()
+    .MinimumLevel.Information()
+    .CreateLogger();
+
+// Add services to the container.
+builder.Host.UseSerilog();
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 // Configure the HTTP request pipeline.
 
 app.UseAuthorization();
